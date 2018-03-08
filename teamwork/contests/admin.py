985534683c004f9ad268fcdb4c95c50bbd929227
africamonkey@github.com <africamonkey@github.com>
2018-03-08 21:12:58 +0800

    Update contest Application

--- teamwork/contests/admin.py
+++ teamwork/contests/admin.py
@@ -2,4 +2,5 @@ from django.contrib import admin
 
 from contests.models import Contest
 
+admin.site.register(Contest)
 # Register your models here.
--- teamwork/contests/models.py
+++ teamwork/contests/models.py
@@ -1,24 +1,24 @@
 from django.db import models
 
 class Contest(models.Model):
-	cid = models.AutoField(primary_key = True)
 	name = models.CharField(max_length = 200)
 	date = models.DateTimeField()
 	NUM_OF_PROBLEM = (
-		('5 - E', 5),
-		('6 - F', 6),
-		('7 - G', 7),
-		('8 - H', 8),
-		('9 - I', 9),
-		('10 - J', 10),
-		('11 - K', 11),
-		('12 - L', 12),
-		('13 - M', 13),
+		(5, '5 - E'),
+		(6, '6 - F'),
+		(7, '7 - G'),
+		(8, '8 - H'),
+		(9, '9 - I'),
+		(10, '10 - J'),
+		(11, '11 - K'),
+		(12, '12 - L'),
+		(13, '13 - M'),
 	)
 	num_of_problem = models.IntegerField(
 		choices = NUM_OF_PROBLEM,
-		default = '5 - E',
+		default = 5,
 	)
 
 	def __str__(self):
-		return str(cid) + ' ' + self.name
+		return self.name
+
