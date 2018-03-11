e5e22b24f49d62e7acb0d3ad9753bc5c1273c6fc
africamonkey@github.com <africamonkey@github.com>
2018-03-11 21:03:30 +0800

    add 'contests.display_contest', 'contests.edit_contest' contests app completed!

--- teamwork/contests/views.py
+++ teamwork/contests/views.py
@@ -1,4 +1,4 @@
-from django.shortcuts import render
+from django.shortcuts import render, get_object_or_404
 from django.http import HttpResponseRedirect
 from django.urls import reverse
 from .models import Contest
@@ -20,3 +20,21 @@ def add_contest(request):
 	
 	context = {'form': form}
 	return render(request, 'contests/add_contest.html', context)
+
+def display_contest(request, contest_id):
+	contest = get_object_or_404(Contest, pk=contest_id)
+	context = {'contest': contest}
+	return render(request, 'contests/display_contest.html', context)
+
+def edit_contest(request, contest_id):
+	contest = get_object_or_404(Contest, pk=contest_id)
+	if request.method != 'POST':
+		form = ContestForm(instance = contest)
+	else:
+		form = ContestForm(instance = contest, data = request.POST)
+		if form.is_valid():
+			form.save()
+			return HttpResponseRedirect(reverse('contests:display_contest', args=[contest.id]))
+	
+	context = {'contest': contest, 'form': form}
+	return render(request, 'contests/edit_contest.html', context)
