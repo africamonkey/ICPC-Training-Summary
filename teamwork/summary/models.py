c1b8211d0c8abe4b1df367320f1f2d5be7900061
africamonkey@github.com <africamonkey@github.com>
2018-03-18 20:17:54 +0800

    migrate database, add simple_history

--- teamwork/summary/models.py
+++ teamwork/summary/models.py
@@ -2,6 +2,7 @@ from django.db import models
 
 from users.models import UserProfile
 from contests.models import Contest
+from simple_history.models import HistoricalRecords
 
 
 class Status(models.Model):
@@ -10,6 +11,7 @@ class Status(models.Model):
     summary = models.TextField()
     ac_status = models.BigIntegerField(default=0, null=True, blank=True)
     contributor = models.BigIntegerField(default=0, null=True, blank=True)
+    history = HistoricalRecords()
 
     class Meta:
         verbose_name_plural = 'statuses'
